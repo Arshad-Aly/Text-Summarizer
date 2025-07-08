8da0e0ca78245ca76ca2a87ae8f9db9fb55ca541
Arshad-Aly <syedarshadali188@gmail.com>
2025-07-08 21:48:59 +0530

    added requiremnets.txt

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f: ## To publish as PYPI package, add a README.md file
+    long_description = f.read()
+
+__version__= "0.0.0"
+
+REPO_NAME = 'Text-Summarizer'
+AUTHOR_USER_NAME = 'Arshad-Aly'
+SRC_REPO = 'Text-Summarizer'
+AUTHOR_EMAIL = 'syedarshadali188@gmail.com'
+
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Python package for text summarization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{SRC_REPO}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{SRC_REPO}/issues",
+    },
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+)
